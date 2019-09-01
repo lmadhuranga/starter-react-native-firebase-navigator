bff613cf589b72b23ec10d88fa41c8d3b258bc95
Madhuranga Sendheera <lilan.maduranga@gmail.com>
2019-09-01 12:35:15 +0530

    initi auth ok

---
+++ screens/LoginPage.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { View, TextInput, Button, ActivityIndicator, Text } from 'react-native'; 
+import firebase from 'react-native-firebase';
+
+class LoginPage extends Component {
+  constructor() {
+    super();
+    this.ref = firebase.firestore().collection('posts');
+    this.firestoreUnsubscriber = null;
+    this.authUnsubscriber = null;
+    this.state = {
+      emailValue: '',
+      passwordValue: '',
+    };
+  }
+
+  onChangeLogin = (e, type) => {
+    this.setState({ [`${type}Value`]: e });
+  };
+
+  onLogin = async () => {
+    try {
+      const response = await firebase
+        .auth()
+        .signInWithEmailAndPassword(this.state.emailValue, this.state.passwordValue);
+      console.log(response);
+      if (response.error) {
+        throw new Error(response);
+      } 
+    } catch (error) {
+      console.log(error); 
+    }
+  };
+
+  render() {
+
+    return (
+      <View>
+        <TextInput
+          autoCapitalize="none"
+          autoCorrect={false}
+          autoFocus
+          value={this.state.emailValue}
+          onChangeText={e => this.onChangeLogin(e, 'email')}
+          keyboardType="email-address"
+          placeholder="partner@benestudio.co"
+        />
+        <TextInput
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry
+          value={this.state.passwordValue}
+          onChangeText={e => this.onChangeLogin(e, 'password')}
+          placeholder="Password"
+        />
+        <Button title="Login" onPress={this.onLogin} />  
+      </View>
+    );
+  }
+} 
+
+export default LoginPage;
